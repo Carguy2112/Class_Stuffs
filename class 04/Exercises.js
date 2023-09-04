59db5a6bbd8b614d3e292a811dc7f66e12e23577
Tom Boyd <carguy2112@yahoo.com>
2023-09-03 21:58:29 -0500

    Exercises

--- class 04/Exercises.js
+++ class 04/Exercises.js
@@ -54,3 +54,94 @@ function strInter(str){
 strInter('words');
 
 //Exercise 11
+let user = {
+    name: 'Bill',
+    age: 67,
+    occupation: 'CEO'};
+
+function logUser(person){
+    console.log(`${person.name} is ${person.age} years old and the former ${person.occupation} of Microsoft.`)
+};
+
+//Exercise 12
+function sortByKeys(input){
+    let sortedArray = (Object.fromEntries(Object.entries(input).sort()));
+
+    console.log(sortedArray);
+    console.log(Object.keys(sortedArray), Object.values(sortedArray));
+    }
+
+let keysAndValues = { key3: true, key2: false, key1: undefined };    
+sortByKeys(keysAndValues);
+
+//Exercise 13
+function Backwards(string){
+    return string.split("").reverse().join("").split(" ").reverse().join(" ")
+}
+
+let sentence = 'Words in a sentence';
+console.log(Backwards(sentence))
+
+//Exercise 14
+function balancedParentheses(string) {
+    let parentheses = "[]{}()";
+    let stack = [];
+  
+    for(i = 0; character = string[i]; i++) {
+      bracePosition = parentheses.indexOf(character);
+  
+      if(bracePosition === -1) {
+        continue;
+      }
+      if(bracePosition % 2 === 0) {
+        stack.push(bracePosition + 1);
+        } 
+      else if(stack.pop() !== bracePosition) {
+        return false;
+        } 
+    }
+    return stack.length === 0;
+  }
+if (balancedParentheses("[()]{}{[()()]()}")){
+    console.log('balanced')
+    } else {console.log('not balanced')};
+
+//Exercise 15
+function anagramPairs(str1, str2){
+    //Check length
+    if(str1.length !== str2.length){return false};
+
+    let s1 = str1.split("").sort().join("");
+    let s2 = str2.split("").sort().join("");
+
+    return (s1===s2);
+    }
+if (anagramPairs('fired','fried')){
+        console.log('is anagram')
+    } else {console.log('not anagram')}
+
+//Exercise 16
+for (i=1; i<101; i++){
+    if(i%15 == 0) console.log("FizzBuzz");
+    else if (i%3 == 0) console.log("Fizz");
+    else if (i%5 == 0) console.log("Buzz");
+    else console.log(i);
+}
+
+//Exercise 17
+function palindrome(string){
+    let str = string.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/,"")
+    let str2 = str.replace(/\s{1,}/g,"");
+    let len = str2.length;
+    for(i=0; i<len/2; i++){
+        if(str2[i].toUpperCase() !== str2[len-1-i].toUpperCase()){
+            return(str2.toLowerCase() + " is not a palindrome")
+        } 
+    }
+    return(str2.toLowerCase() + " is a palindrome")
+}
+console.log(palindrome('r?a\d aR'))
+
+//Exercise 18
+let str = "the words in a sentence"
+console.log(str.split(/\s/).reverse().join(" "));
