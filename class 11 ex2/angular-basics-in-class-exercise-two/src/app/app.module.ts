573d57290f76cb50f43c655223a1cb7acc5d6c3e
Tom Boyd <carguy2112@yahoo.com>
2023-09-30 14:57:04 -0500

    Class 11 exercises

---
+++ class 11 ex2/angular-basics-in-class-exercise-two/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [AppComponent, ArticleComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ class 11 ex2/angular-basics-in-class-exercise-two/src/app/article/article.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-article",
+  templateUrl: "./article.component.html",
+  styleUrls: ['./article.component.css']
+
+})
+
+export class ArticleComponent{
+  title:string = 'Whatever you want'
+  content:string = "Some content goes here. The instructions reference 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, assumenda. Quae, eveniet sint ullam enim explicabo sapiente alias voluptas doloremque delectus nam reprehenderit. Illum ipsum necessitatibus corporis ad nemo est.' but I think it would be better with 'Some content goes here'."
+}
