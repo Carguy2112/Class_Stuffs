dec498482474efedee33c3427a53186c24eed1af
Tom Boyd <carguy2112@yahoo.com>
2023-10-01 12:25:49 -0500

    c1 ex1 bonus

--- class 11 ex1/angular-basics-in-class-exercise-one/src/app/app.module.ts
+++ class 11 ex1/angular-basics-in-class-exercise-one/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
