78059e4609f125151b2b07061208a6a792de0ea3
Tom Boyd <carguy2112@yahoo.com>
2023-11-12 13:42:20 -0600

    vehicle card

--- Project/Car-Country/src/app/shared/services/inventory.service.ts
+++ Project/Car-Country/src/app/shared/services/inventory.service.ts
@@ -11,8 +11,30 @@ export class InventoryService {
     new Vehicle(
       2018,
       'GMC',
-      'Sierra SLT Z71',
+      'Sierra',
+      'SLT Z71',
+      'White',
+      'Black Leather',
+      'EcoTec3 5.3L V8',
+      'Automatic',
+      '4X4',
+       149091,
+      '28,900',
       'https://cf-img.autorevo.com/2018-gmc-sierra-1500-jackson-mo-7245436/724x724/2873669-40-revo.jpg?_=1695222298'
+    ),
+    new Vehicle(
+      2014,
+      'BMW',
+      '3 Series 335i',
+      'xDrive Gran Turismo',
+      'Black',
+      'Black Leather',
+      '3.0L Turbo I6',
+      'Automatic',
+      'Awd',
+       51725,
+      '19,900',
+      'https://cf-img.autorevo.com/2014-bmw-3-series-jackson-mo-7247475/724x724/2875869-43-revo.jpg?_=1699543357'
     )
   ];
 
--- Project/Car-Country/src/app/vehicle/vehicle.model.ts
+++ Project/Car-Country/src/app/vehicle/vehicle.model.ts
@@ -3,7 +3,15 @@ export class Vehicle {
     public year: number,
     public make: string,
     public model: string,
-    public coverImagePath: string
+    public trim: string,
+    public exterior: string,
+    public interior: string,
+    public engine: string,
+    public trans: string,
+    public drive: string,
+    public miles: number,
+    public price: string,
+    public coverImagePath?: string
 
   ){}
 }
