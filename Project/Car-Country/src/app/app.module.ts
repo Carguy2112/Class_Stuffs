86ade00bb63acc6797c71ec13796eff12391b326
Tom Boyd <120299720+Carguy2112@users.noreply.github.com>
2023-11-13 20:25:20 -0600

    vehicle details

--- Project/Car-Country/src/app/app.module.ts
+++ Project/Car-Country/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { VehicleEditComponent } from './vehicle/vehicle-edit/vehicle-edit.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
-import { HomeComponent } from './home/home.component';
 import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
+import { VehicleDetailsComponent } from './vehicle/vehicle-details/vehicle-details.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { InventoryListComponent } from './inventory/inventory-list/inventory-lis
     VehicleEditComponent,
     NavigationComponent,
     HomeComponent,
-    InventoryListComponent
+    InventoryListComponent,
+    VehicleDetailsComponent
   ],
   imports: [
     BrowserModule,
--- Project/Car-Country/src/app/inventory/inventory.component.ts
+++ Project/Car-Country/src/app/inventory/inventory.component.ts
@@ -15,8 +15,4 @@ export class InventoryComponent {
    constructor(
     private inventoryService: InventoryService,
    ){}
-
-   ngOnInit() {
-
-   }
 }
