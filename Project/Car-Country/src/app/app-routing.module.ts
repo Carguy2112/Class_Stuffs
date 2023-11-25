6017b9a49f97b478bf0d45dcf6876ebf2680de72
Tom Boyd <carguy2112@yahoo.com>
2023-11-25 14:44:44 -0600

    layout changes to vehicle details

--- Project/Car-Country/src/app/app-routing.module.ts
+++ Project/Car-Country/src/app/app-routing.module.ts
@@ -1,39 +1,41 @@
-import { Route, RouterModule } from "@angular/router";
-import { NgModule } from "@angular/core";
+import { Route, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-import { HomeComponent } from "./home/home.component";
-import { InventoryComponent } from "./inventory/inventory.component";
-import { VehicleComponent } from "./vehicle/vehicle.component";
-import { VehicleEditComponent } from "./vehicle/vehicle-edit/vehicle-edit.component";
-import { VehicleDetailsComponent } from "./vehicle/vehicle-details/vehicle-details.component";
-
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { VehicleEditComponent } from './vehicle/vehicle-edit/vehicle-edit.component';
+import { VehicleDetailsComponent } from './vehicle/vehicle-details/vehicle-details.component';
 
 const appRoutes: Route[] = [
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'inventory',
-    component: InventoryComponent
+    component: InventoryComponent,
   },
   {
     path: 'vehicle',
     children: [
       {
-        path: '',
-        component: VehicleComponent,
-      },
-      {
         path: ':id/edit',
-        component: VehicleEditComponent
+        component: VehicleEditComponent,
       },
       {
         path: ':id',
-        component: VehicleDetailsComponent
+        component: VehicleDetailsComponent,
       },
-    ]
-  }
+      {
+        path: '',
+        component: VehicleComponent,
+      },
+    ],
+  },
+  {
+    path: '**', redirectTo: '/home', pathMatch: 'full'
+  },
 ];
 
 @NgModule({
