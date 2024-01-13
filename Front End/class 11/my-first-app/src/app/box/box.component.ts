edede4aead6f549ca2e7f51a251d56cebd67f539
Tom Boyd <carguy2112@yahoo.com>
2024-01-13 12:55:29 -0600

    update home pc

--- Front End/class 11/my-first-app/src/app/box/box.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-box',
-  templateUrl: './box.component.html',
-  styleUrls: ['./box.component.css'],
-})
-export class BoxComponent {}
