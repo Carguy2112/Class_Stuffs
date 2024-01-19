1a55cffe4b9f38e122289f0ce9c50180ce155ab9
Tom Boyd <120299720+Carguy2112@users.noreply.github.com>
2024-01-18 19:15:34 -0600

    update laptop

--- be-03/ex5/app.js
+++ be-03/ex5/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+
+const path = require('path');
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname, './home.html'))
+    res.sendFile(path.resolve(__dirname,'./home.html'))
 })
 
 app.get('/detail',(req, res)=>{
