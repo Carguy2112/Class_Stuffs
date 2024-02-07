dd11ade64d0b3e6395809f932ca7e79db70c4511
Tom Boyd <carguy2112@yahoo.com>
2024-02-06 22:02:39 -0600

    JWT init

---
+++ Backend/JWT_Basics/controllers/main.js
@@ -0,0 +1,20 @@
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        throw new CustomAPIError("Please provide email and password", 400);
+    }
+    res.send("fake login stuffs");
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `hello`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+};
+
+module.exports = { login, dashboard };
