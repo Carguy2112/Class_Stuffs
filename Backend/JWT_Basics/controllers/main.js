028ed5b056ac95e4cee26a7845a009e1b7a05f05
Tom Boyd <carguy2112@yahoo.com>
2024-02-07 18:52:28 -0600

    JWT Basics complete

--- Backend/JWT_Basics/controllers/main.js
+++ Backend/JWT_Basics/controllers/main.js
@@ -1,18 +1,26 @@
-const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        throw new CustomAPIError("Please provide email and password", 400);
+        throw new BadRequestError("Please provide email and password");
     }
-    res.send("fake login stuffs");
+
+    const id = new Date().getDate();
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+    res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
+    console.log(req.user);
     const luckyNumber = Math.floor(Math.random() * 100);
     res.status(200).json({
-        msg: `hello`,
+        msg: `hello ${req.user.username}`,
         secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
     });
 };
