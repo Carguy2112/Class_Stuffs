3fab28083bf3231b5df78582be130791367c88c6
Tom Boyd <carguy2112@yahoo.com>
2024-02-12 17:34:27 -0600

    first commit

---
+++ Backend/jobsAPI/controllers/jobs.js
@@ -0,0 +1,23 @@
+const getAllJobs = async (req, res) => {
+    res.send("get all jobs");
+};
+const getJob = async (req, res) => {
+    res.send("get single job");
+};
+const createJob = async (req, res) => {
+    res.json(req.user);
+};
+const updateJob = async (req, res) => {
+    res.send("update job");
+};
+const deleteJob = async (req, res) => {
+    res.send("delete job");
+};
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+};
